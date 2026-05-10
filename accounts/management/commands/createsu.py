82a4a381924a65051c76f0caf16f351aad2bd26e
Vaibhav Gupta <gvaibhav5941@gmail.com>
2026-05-10 17:28:44 +0530

    Update createsu.py

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -2,28 +2,32 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
 
-User = get_user_model()
-
 class Command(BaseCommand):
     help = "Create superuser if not exists"
 
     def handle(self, *args, **kwargs):
+        User = get_user_model()
 
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
         password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
         first_name = os.environ.get("DJANGO_SUPERUSER_FIRSTNAME", "Admin")
         last_name = os.environ.get("DJANGO_SUPERUSER_LASTNAME", "User")
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                first_name=first_name,
-                last_name=last_name,
-                username=username,
-                email=email,
-                password=password
-            )
-            self.stdout.write(self.style.SUCCESS("Superuser created"))
-        else:
-            self.stdout.write("Superuser already exists")
+        if not email or not password:
+            self.stdout.write(self.style.WARNING("Env vars missing, skipping."))
+            return
+
+        # Email OR username dono mein se koi bhi exist kare toh skip karo
+        if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser already exists, skipping.")
+            return
+
+        User.objects.create_superuser(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email,
+            password=password
+        )
+        self.stdout.write(self.style.SUCCESS("Superuser created"))
