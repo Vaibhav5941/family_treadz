ef75329fad045d03bf21a39ef9f0b7aa45293f9a
Vaibhav Gupta <gvaibhav5941@gmail.com>
2026-05-10 18:00:05 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ from django.db.models import Avg, Count
 # Create your models here.
 
 class Product(models.Model):
+    product_id      = models.CharField(max_length=20, unique=True, blank=True, null=True)
     product_name    = models.CharField(max_length=200, unique=True)
     slug            = models.SlugField(max_length=200, unique=True)
     description     = models.TextField(max_length=500, blank=True)
